a424ee838b76fb1ae717e642d82b6ebcd3cbcac2
Jeremy Peake <peakeelectronicinnovation@gmail.com>
2025-02-08 13:09:25 +1300

    Fixed type conversion issue with ADC, improved 0 RPM handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,46 @@
 void pulse_ISR(void);
 void handle_ADC(void);
 void handle_motor_pwr(void);
+bool set_rpm(float rpm_val);
+bool check_pulse_timeout(void);
 
 // ----------------------------Setup ----------------------------
 void setup() {
+    if (debug) {
+        SerialDebug.begin(115200);
+        while (!SerialDebug);
+        SerialDebug.println("Starting pico sensor interface in debug mode.");
+    }
     analogReadResolution(12);
-    // Init power monitoring - INA226?
-
+    // TODO: Init power monitoring - INA226?
+    /*Wire.setSDA(PIN_SDA);
+    Wire.setSCL(PIN_SCL);*/
     // ...
+    
+    // DEBUG - Setup modbus serial port on UART0 (debug mode only - frees up USB for uploads and debugging)
+    Serial1.setTX(PIN_SDA);
+    Serial1.setRX(PIN_SCL);
+    // END DEBUG
+
     modbus.configureHoldingRegisters(holdingRegisters, 10);
     modbus.begin(1, 115200);        // Modbus slave ID 1, baud rate 115200
     adcLastMillis = millis();
-    lastMicros = micros();
+    rpm_lastMicros = micros();
+    
+    core0_ready = true;
+    while (!core1_ready);
+    if (debug) SerialDebug.println("Setup complete.");
+
+    if (debug) SerialDebug.printf("A0: %u, A1: %u, A2: %u\n", analogRead(PIN_ADC_0), analogRead(PIN_ADC_1), analogRead(PIN_ADC_2));
 }
 
 void setup1() {
     sensorMutex = xSemaphoreCreateMutex();
     pinMode(PIN_PULSE_INPUT, INPUT_PULLUP);
     attachInterrupt(PIN_PULSE_INPUT, pulse_ISR, FALLING);
+    if (debug) SerialDebug.println("Core 1 setup complete.");
+    core1_ready = true;
+    while (!core0_ready);
 }
 
 // ----------------------------Loop ----------------------------
@@ -31,42 +54,49 @@ void loop() {
 }
 
 void loop1() {
-    delay(1);
+    delay(300);
+    if (rpmUpdated) set_rpm(rpm);
+    else (check_pulse_timeout());
 }
 
 // ----------------------- ISR callbacks -----------------------
 
 void pulse_ISR(void) {
-    uint32_t ts = micros();
-    uint32_t period = ts - lastMicros;
-    lastMicros = ts;
-    if (xSemaphoreTake(sensorMutex, 0) == pdTRUE) {
-        if (period < 60000000) sensors.pulse_rate = 1000000.0 / period; // Ignore pulse durations longer than 60 seconds
-        else sensors.pulse_rate = 0.0;
-        xSemaphoreGive(sensorMutex);
-    }
+    rpm_timestamp = micros();
+    rpm_period = rpm_timestamp - rpm_lastMicros;
+    rpm_lastMicros = rpm_timestamp;
+    if (rpm_period < 60000000) rpm = (1000000.0 / rpm_period) * 60;
+    else rpm = 0.0;  // Ignore pulse durations longer than 60 seconds
+    rpmUpdated = true;
 }
 
 void handle_ADC(void) {
     adcLastMillis = millis();
-    float samples[3];
+    uint32_t samples[3] = {0, 0, 0};
     // Sample each channel 16 times and average
     for (uint8_t s = 0; s < SAMPLES; s++) {
         for (uint8_t ch = 0; ch < 3; ch++) {
             samples[ch] += analogRead(adc_ch[ch]);
         }
     }
-    for (uint8_t ch = 0; ch < 3; ch++) samples[ch] /= SAMPLES;
+    //if (debug) SerialDebug.printf("16x oversampled - A0: %u, A1: %u, A2: %u\n", samples[0], samples[1], samples[2]);
+    for (uint8_t ch = 0; ch < 3; ch++) {
+        samples[ch] /= SAMPLES;
+        if (samples[ch] < 20) samples[ch] = 0.0; // Ignore noise
+    }
 
     // Calculate effective mA measurement for each channel and store to holding registers
     if (xSemaphoreTake(sensorMutex, 0) == pdTRUE) {
-        sensors.ch0_mA = samples[0] * mA_PER_BIT;
-        sensors.ch1_mA = samples[1] * mA_PER_BIT;
-        sensors.ch2_mA = samples[2] * mA_PER_BIT;
+        sensors.ch0_mA = (float)samples[0] * mA_PER_BIT;
+        sensors.ch1_mA = (float)samples[1] * mA_PER_BIT;
+        sensors.ch2_mA = (float)samples[2] * mA_PER_BIT;
         memcpy(holdingRegisters, &sensors, sizeof(sensors_t));
         xSemaphoreGive(sensorMutex);
     }
-    
+    if (debug) {
+        uint32_t adc_time = millis() - adcLastMillis;
+        SerialDebug.printf("ADC oversampled %ix, time: %ums\n", SAMPLES, adc_time);
+    }
 }
 
 void handle_motor_pwr(void) {
@@ -80,4 +110,21 @@ void handle_motor_pwr(void) {
         sensors.motor_mA = motor_current;
         xSemaphoreGive(sensorMutex);
     }
+}
+
+bool set_rpm(float rpm_val) {
+    if (xSemaphoreTake(sensorMutex, 0) == pdTRUE) {
+        sensors.rpm = rpm_val;
+        xSemaphoreGive(sensorMutex);
+        rpmUpdated = false;
+        return true;
+    }
+    return false;
+}
+
+bool check_pulse_timeout(void) {
+    if (micros() - rpm_lastMicros < PULSE_TIMEOUT) return false;
+    rpm = 0.0;
+    set_rpm(0.0);
+    return true;
 }
--- src/sys_init.h
+++ src/sys_init.h
@@ -1,31 +1,42 @@
 #include "Arduino.h"
+#include "Wire.h"
 #include "ModbusRTUSlave.h"
 #include "FreeRTOS.h"
 #include "semphr.h"
 
 // Pins
-#define PIN_ADC_0   26
-#define PIN_ADC_1   27
-#define PIN_ADC_2   28
+#define PIN_ADC_0       26
+#define PIN_ADC_1       27
+#define PIN_ADC_2       28
 #define PIN_PULSE_INPUT 22
+#define PIN_SDA         16
+#define PIN_SCL         17
+
+// Serial
+#define SerialDebug Serial          // USB serial
+#define SerialModbus Serial1        // UART 0
 
 // 4-2-mA calcs
-#define SAMPLES  16
-#define ADC_INTERVAL 100
-#define mA_PER_BIT 0.00805664062
+#define SAMPLES  128                // Number of samples per channel measurement (about 100µs per sample)
+#define ADC_INTERVAL 100            // Time in milliseconds between ADC measurements
+#define mA_PER_BIT 0.00805664062    // mA per LSB constant (calculated for 100­Ω shunt resistor, 12-bit ADC, 3.3V reference voltage)
 
+// Motor power update interval (milliseconds)
 #define MOTOR_INTERVAL 500
 
+// Stirrer RPM pulse timeout (microseconds)
+#define PULSE_TIMEOUT 10000000  // 10 seconds
+
 // Lib objects
-ModbusRTUSlave modbus(Serial);
-SemaphoreHandle_t sensorMutex = NULL;
+ModbusRTUSlave modbus(SerialModbus);
+SemaphoreHandle_t sensorMutex = NULL;       // Mutex so both cores can safely access sensor data
 
 // Globals
 struct sensors_t {
     float ch0_mA = 4.0;
     float ch1_mA = 8.596;
     float ch2_mA = 16.25736;
-    float pulse_rate = 0.0;
+    float rpm = 0.0;
     float motor_mA = 0.0;
 };
 
@@ -36,6 +47,16 @@ uint16_t holdingRegisters[100];
 uint32_t adc_ch[3] = {PIN_ADC_0, PIN_ADC_1, PIN_ADC_2};
 
 // Timing
-uint32_t lastMicros;
+uint32_t rpm_timestamp;
+uint32_t rpm_period;
+uint32_t rpm_lastMicros;
 uint32_t adcLastMillis;
 uint32_t motorLastMillis;
+
+// RPM
+float rpm = 0.0;
+bool rpmUpdated = false;
+
+// Flags
+bool debug = true;
+bool core0_ready = false, core1_ready = false;
